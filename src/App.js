73e02414e10200b7ce3b7b18852718b4a723afc9
kiruthika5801 <kiruthikabalu87@gmail.com>
2024-12-25 14:48:42 +0530

    space

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import Header from './component/Header';
+
+import "../src/App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HeaderNav from './component/HeaderNav';
+
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <HeaderNav/>
+     
+     
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Header.jsx
@@ -0,0 +1,60 @@
+import { Container, Row, Col } from "react-bootstrap";
+import logo from '../images/mains.png'
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { FiPhone } from "react-icons/fi";
+
+
+function Header() {
+  return (
+    <div>
+      <Container fluid>
+        <Row className="h-auto" >
+          <Col className=" one ">
+            <Row className="left w-auto me-5">
+
+              <Col className="text-end w-auto" lg={9}>
+                <Row><span >Call Us</span></Row>
+                <Row><span >800-123-4567</span></Row>
+
+              </Col>
+              <Col lg={3} className="w-auto">
+                <Row className="image">
+                  
+                  <FiPhone className="phone mt-3" />
+                </Row>
+
+              </Col>
+
+            </Row>
+
+          </Col>
+          <Col className="text-center  two"><img className="logo" src={logo} alt="tie-tales" /></Col>
+          <Col className=" three">
+            <Row className="left ms-5 w-auto">
+
+
+              <Col lg={3} className=" w-auto">
+                <Row className="image">
+                  <HiOutlineShoppingBag className="bag mt-3" />
+                </Row>
+
+              </Col>
+              <Col className="w-auto" lg={9}>
+
+                <Row><span >your cart:</span></Row>
+                <Row><span >(2)-$120.00</span></Row>
+
+              </Col>
+
+            </Row>
+
+
+          </Col>
+        </Row>
+
+      </Container>
+    </div>
+
+  )
+};
+export default Header;
