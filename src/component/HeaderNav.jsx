567786b206eecd2bba7bf4cf70d85f31e4c1d999
kiruthika5801 <kiruthikabalu87@gmail.com>
2024-12-27 08:27:15 +0530

    router

--- src/component/HeaderNav.jsx
+++ src/component/HeaderNav.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Row, Col, Container, Navbar, Nav, NavDropdown, Form, Button, Offcanvas } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
@@ -25,14 +26,14 @@ function HeaderNav() {
                   </Offcanvas.Header> */}
                   <Offcanvas.Body>
                     <Nav className="justify-content-end flex-grow-1 ps-5">
-                      <Nav.Link href="#home">HOME </Nav.Link>
-                      <Nav.Link href="#link">ABOUT </Nav.Link>
-                      <Nav.Link href="#home">FEATURES</Nav.Link>
-                      <Nav.Link href="#link">SHOP</Nav.Link>
-                      <Nav.Link href="#home">TEAM</Nav.Link>
-                      <Nav.Link href="#link">SERVICES</Nav.Link>
-                      <Nav.Link href="#link">SHOP</Nav.Link>
-                      <Nav.Link href="#link">CONTACTS</Nav.Link>
+                      <Nav.Link as={Link} to="#home">HOME </Nav.Link>
+                      <Nav.Link as={Link} to="#link">ABOUT </Nav.Link>
+                      <Nav.Link as={Link} to="#home">FEATURES</Nav.Link>
+                      <Nav.Link as={Link} to="#link">SHOP</Nav.Link>
+                      <Nav.Link as={Link} to="#home">TEAM</Nav.Link>
+                      <Nav.Link as={Link} to="#link">SERVICES</Nav.Link>
+                      <Nav.Link as={Link} to="#link">SHOP</Nav.Link>
+                      <Nav.Link as={Link} to="#link">CONTACTS</Nav.Link>
 
                     </Nav>
 
