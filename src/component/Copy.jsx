7731fefb12a8a49b4c3ba98eef3986fc5de8a683
kiruthika5801 <kiruthikabalu87@gmail.com>
2025-02-04 22:09:40 +0530

    till

--- src/component/Copy.jsx
+++ src/component/Copy.jsx
@@ -1,162 +1,61 @@
-// import Button from 'react-bootstrap/Button';
-// import Container from 'react-bootstrap/Container';
-// import Form from 'react-bootstrap/Form';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-// import Offcanvas from 'react-bootstrap/Offcanvas';
+import React from "react";
+import "../App.css"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import dividers from "../images/divider_title.png";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-// function Copy() {
-//   return (
-//     <>
-//       {[ 'lg'].map((expand) => (
-//         <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
-//           <Container fluid>
-//             <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-//             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-//             <Navbar.Offcanvas
-//               id={`offcanvasNavbar-expand-${expand}`}
-//               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-//               placement="start"
-//             >
-//               <Offcanvas.Header closeButton>
-//                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-//                   Offcanvas
-//                 </Offcanvas.Title>
-//               </Offcanvas.Header>
-//               <Offcanvas.Body>
-//                 <Nav className="justify-content-end flex-grow-1 pe-3">
-//                   <Nav.Link href="#action1">Home</Nav.Link>
-//                   <Nav.Link href="#action2">Link</Nav.Link>
-//                   <NavDropdown
-//                     title="Dropdown"
-//                     id={`offcanvasNavbarDropdown-expand-${expand}`}
-//                   >
-//                     <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-//                     <NavDropdown.Item href="#action4">
-//                       Another action
-//                     </NavDropdown.Item>
-//                     <NavDropdown.Divider />
-//                     <NavDropdown.Item href="#action5">
-//                       Something else here
-//                     </NavDropdown.Item>
-//                   </NavDropdown>
-//                 </Nav>
-//                 <Form className="d-flex">
-//                   <Form.Control
-//                     type="search"
-//                     placeholder="Search"
-//                     className="me-2"
-//                     aria-label="Search"
-//                   />
-//                   <Button variant="outline-success">Search</Button>
-//                 </Form>
-//               </Offcanvas.Body>
-//             </Navbar.Offcanvas>
-//           </Container>
-//         </Navbar>
-//       ))}
-//     </>
-//   );
-// }
-
-// export default Copy;
-
-// import React from 'react';
-// import { Nav,  NavDropdown } from 'react-bootstrap';
-
-// function Copy() {
-//   return (
-//     <Nav className="justify-content-end flex-grow-1 ps-5">
-//     <Nav.Link href="#home">HOME</Nav.Link>
-//     <Nav.Link href="#link">ABOUT</Nav.Link>
-//     <Nav.Link href="#home">FEATURES</Nav.Link>
-//     <Nav.Link href="#link">SHOP</Nav.Link>
-//     <Nav.Link href="#home">TEAM</Nav.Link>
-//     {/* Dropdown for Services */}
-//     <NavDropdown title="SERVICES" id="nav-dropdown-services">
-//       <NavDropdown.Item href="#service1">Service 1</NavDropdown.Item>
-//       <NavDropdown.Item href="#service2">Service 2</NavDropdown.Item>
-//       <NavDropdown.Item href="#service3">Service 3</NavDropdown.Item>
-//     </NavDropdown>
-//     <Nav.Link href="#link">CONTACTS</Nav.Link>
-//   </Nav>
-//   );
-// }
-// import Carousel from 'react-bootstrap/Carousel';
-// import ExampleCarouselImage from 'components/ExampleCarouselImage';
-
-// function Copy(){
-//   return(
-//     <div>
-//        <Carousel interval={5000} controls={false}>
-//       <Carousel.Item>
-//         <ExampleCarouselImage text="First slide" />
-//         <Carousel.Caption>
-//           <h3>First slide label</h3>
-//           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//       <Carousel.Item>
-//         <ExampleCarouselImage text="Second slide" />
-//         <Carousel.Caption>
-//           <h3>Second slide label</h3>
-//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//       <Carousel.Item>
-//         <ExampleCarouselImage text="Third slide" />
-//         <Carousel.Caption>
-//           <h3>Third slide label</h3>
-//           <p>
-//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-//           </p>
-//         </Carousel.Caption>
-//       </Carousel.Item>
-//     </Carousel>
-
-//     </div>
-//   )
-// };
-// export default Copy;
-
-import React from 'react';
-import slideOne from "../images/slide1.jpg";
-import slideTwo from "../images/slide2.jpg";
-import slideThree from "../images/image.png";
-
-function Main() {
+function Shop() {
+    const products = [
+        { id: 1, title: "Flower Decor", price: "$55.00", image: "https://via.placeholder.com/150" },
+        { id: 2, title: "Product 2", description: "This is product 2", image: "https://via.placeholder.com/150" },
+        { id: 3, title: "Product 3", description: "This is product 3", image: "https://via.placeholder.com/150" },
+        { id: 4, title: "Product 4", description: "This is product 4", image: "https://via.placeholder.com/150" },
+        { id: 5, title: "Product 5", description: "This is product 5", image: "https://via.placeholder.com/150" },
+        { id: 6, title: "Product 6", description: "This is product 6", image: "https://via.placeholder.com/150" },
+    ];
     return (
-        <div className="swiffy-slider slider-nav-autoplay slider-indicators-round slider-nav-animation slider-nav-animation-appear" data-slider-nav-autoplay-interval="5000">
-            <ul className="slider-container">
-                <li>
-                    <img src={slideOne} style={{ width: "100%", height: "500px" }} alt="First slide" />
-                    <div className="slider-content">
-                        <h3>First Slide Label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </div>
-                </li>
-                <li>
-                    <img src={slideTwo} style={{ width: "100%", height: "500px" }} alt="Second slide" />
-                    <div className="slider-content">
-                        <h3>Second Slide Label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                </li>
-                <li>
-                    <img src={slideThree} style={{ width: "100%", height: "500px" }} alt="Third slide" />
-                    <div className="slider-content">
-                        <h3>Third Slide Label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </div>
-                </li>
-            </ul>
-            <button type="button" className="slider-nav"></button>
-            <button type="button" className="slider-nav slider-nav-next"></button>
-           
+        <div>
+            <Container fluid>
+                <Row className="justify-content-md-center shop">
+                    <Col className="shop-main" xs lg={9}>
+                        <Row className='w-head'>
+                            <Col lg={12} xs={10} className='wel-one'> Shop Our Latest Styles</Col>
+                        </Row>
+                        <Row className='w-para'>
+                            <Col lg={12} xs={12} className='wel-two'>shop</Col>
+                        </Row>
+                        <Row className='w-divider' >
+                            <Col className='d-flex justify-content-center align-items-center' ><img className='d-images' src={dividers} alt="divider-title" /></Col>
+                        </Row>
+                        <Row className='shopBox'>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title></Card.Title>
+                                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                    <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                    </Card.Text>
+                                    <Card.Link href="#">Card Link</Card.Link>
+                                    <Card.Link href="#">Another Link</Card.Link>
+                                </Card.Body>
+                            </Card>
+
+
+                        </Row>
+
+
+                    </Col>
+
+
+                </Row>
+
+            </Container>
+
         </div>
-    );
-}
-
-export default Main;
-
+    )
+};
+export default Shop;
--- src/component/Shop.jsx
+++ src/component/Shop.jsx
@@ -1,49 +1,62 @@
 import React from "react";
-import "../App.css"
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import "../App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card"; 
+import Button from 'react-bootstrap/Button';
 import dividers from "../images/divider_title.png";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function Shop() {
     const products = [
-        { id: 1, title: "Flower Decor", price:"$55.00" , image: "https://via.placeholder.com/150" },
-        { id: 2, title: "Product 2", description: "This is product 2", image: "https://via.placeholder.com/150" },
-        { id: 3, title: "Product 3", description: "This is product 3", image: "https://via.placeholder.com/150" },
-        { id: 4, title: "Product 4", description: "This is product 4", image: "https://via.placeholder.com/150" },
-        { id: 5, title: "Product 5", description: "This is product 5", image: "https://via.placeholder.com/150" },
-        { id: 6, title: "Product 6", description: "This is product 6", image: "https://via.placeholder.com/150" },
-      ];
+        { id: 1, title: "Flower Decor", price: "$55.00",image:require("../images/product1-706x783.jpg")},
+        { id: 2, title: "Wedding Arch", price: "$120.00",image:require("../images/product2-706x783.jpg")},
+        { id: 3, title: "Table Centerpiece", price: "$80.00",image:require("../images/product3-706x783.jpg")},
+        { id: 4, title: "Bridal Bouquet", price: "$45.00",image:require("../images/product4-706x783 (1).jpg")},
+        { id: 5, title: "Fairy Lights", price: "$30.00",image:require("../images/product5-555x615.jpg")},
+        { id: 6, title: "Stage Backdrop", price: "$200.00",image:require("../images/product6-555x615.jpg")},
+    ];
+
     return (
-        <div>
-            <Container fluid>
-                <Row className="justify-content-md-center shop">
-                    <Col className="shop-main"   xs lg={9}>
-                    <Row className='w-head'>
-                            <Col   lg={12} xs={10} className='wel-one'> Shop Our Latest Styles</Col>
-                        </Row>
-                        <Row className='w-para'>
-                            <Col lg={12} xs={12} className='wel-two'>shop</Col>
-                        </Row>
-                        <Row className='w-divider' >
-                            <Col className='d-flex justify-content-center align-items-center' ><img className='d-images' src={dividers} alt="divider-title" /></Col>
-                        </Row>
-                        <Row className='shopBox'>
+        <Container fluid>
+            <Row className="justify-content-md-center shop">
+                <Col className="shop-main" xs lg={9}>
+                    <Row className="w-head">
+                        <Col lg={12} xs={10} className="wel-one">
+                            Shop Our Latest Styles
+                        </Col>
+                    </Row>
+                    <Row className="w-para">
+                        <Col lg={12} xs={12} className="wel-two">Shop</Col>
+                    </Row>
+                    <Row className="w-divider">
+                        <Col className="d-flex justify-content-center align-items-center">
+                            <img className="d-images" src={dividers} alt="divider-title" />
+                        </Col>
+                    </Row>
 
+                    {/* Product Grid */}
+                    <Row className="shopBox">
+                        {products.map((product) => (
+                            <Col key={product.id} md={4} sm={6} xs={12} className="mb-4">
+                                <Card>
+                                    {/* Fix: Add product.image to Card.Img */}
+                                    <Card.Img  className="cardShop-img" variant="top" src={product.image} alt={product.title} />
+                                    <Card.Body>
+                                        <Card.Title className="cardShop-title">{product.title}</Card.Title>
+                                        <Card.Subtitle className="mb-2 text-muted cardShop-price">{product.price}</Card.Subtitle>
+                                        <Button className="shopBtn" variant="primary">ADD TO CART</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
 
-                        </Row>
-
-                       
-                    </Col>
-                    
-                    
-                </Row>
-                
-            </Container>
-
-        </div>
-    )
-};
 export default Shop;
