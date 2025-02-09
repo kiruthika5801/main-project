2d2c724283034a335940a70a40b377bf244dcf1c
kiruthika5801 <kiruthikabalu87@gmail.com>
2025-02-10 00:03:04 +0530

    shop

--- src/component/Shop.jsx
+++ src/component/Shop.jsx
@@ -1,30 +1,21 @@
 import React from "react";
 import "../App.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card"; 
-import Button from 'react-bootstrap/Button';
-import dividers from "../images/divider_title.png";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import dividers from "../images/divider_title.png";
+
 
 function Shop() {
-    const products = [
-        { id: 1, title: "Flower Decor", price: "$55.00",image:require("../images/product1-706x783.jpg")},
-        { id: 2, title: "Wedding Arch", price: "$120.00",image:require("../images/product2-706x783.jpg")},
-        { id: 3, title: "Table Centerpiece", price: "$80.00",image:require("../images/product3-706x783.jpg")},
-        { id: 4, title: "Bridal Bouquet", price: "$45.00",image:require("../images/product4-706x783 (1).jpg")},
-        { id: 5, title: "Fairy Lights", price: "$30.00",image:require("../images/product5-555x615.jpg")},
-        { id: 6, title: "Stage Backdrop", price: "$200.00",image:require("../images/product6-555x615.jpg")},
-    ];
+   
 
     return (
         <Container fluid>
             <Row className="justify-content-md-center shop">
-                <Col className="shop-main" xs lg={9}>
+                <Col lg={10} md={11} sm={9} xs={9} className="shop-main">
+                    {/* Header Section */}
                     <Row className="w-head">
-                        <Col lg={12} xs={10} className="wel-one">
+                        <Col lg={12} xs={12} className="wel-one">
                             Shop Our Latest Styles
                         </Col>
                     </Row>
@@ -36,23 +27,97 @@ function Shop() {
                             <img className="d-images" src={dividers} alt="divider-title" />
                         </Col>
                     </Row>
+                    <Row className="shop-boxOne ">
+                        <Col className="d-flex justify-content-center" lg={4} md={4} sm={4}>
+                            <Card className="card-sWhite">
+                                <div className="card-sImg card-sImgOne">
+                                
+                                </div>
+                               
+                                <Card.Body>
+                                    <Card.Title className="card-head">Flower Decor</Card.Title>
+                                    <Card.Text className="card-price"> $55.00 </Card.Text>
+                                    <Button className="card-shopbtn" variant="primary">ADD TO CART</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="d-flex justify-content-center" lg={4} md={4} sm={4}>
+                            <Card className="card-sWhite">
+                                <div className="card-sImg card-sImgTwo">
+                               
+                                </div>
+                               
+                                <Card.Body>
+                                    <Card.Title className="card-head">Wedding Arch</Card.Title>
+                                    <Card.Text className="card-price"> $65.00 </Card.Text>
+                                    <Button className="card-shopbtn"  variant="primary">ADD TO CART</Button>
+                                </Card.Body>
+                            </Card>
 
-                    {/* Product Grid */}
-                    <Row className="shopBox">
-                        {products.map((product) => (
-                            <Col key={product.id} md={4} sm={6} xs={12} className="mb-4">
-                                <Card>
-                                    {/* Fix: Add product.image to Card.Img */}
-                                    <Card.Img  className="cardShop-img" variant="top" src={product.image} alt={product.title} />
-                                    <Card.Body>
-                                        <Card.Title className="cardShop-title">{product.title}</Card.Title>
-                                        <Card.Subtitle className="mb-2 text-muted cardShop-price">{product.price}</Card.Subtitle>
-                                        <Button className="shopBtn" variant="primary">ADD TO CART</Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        ))}
+                        </Col>
+                        <Col className="d-flex justify-content-center" lg={4} md={4} sm={4} >
+                            <Card className="card-sWhite" >
+                                <div className="card-sImg card-sImgThree">
+                                
+                                </div>
+                                
+                                <Card.Body>
+                                    <Card.Title className="card-head">Table Centerpiece</Card.Title>
+                                    <Card.Text className="card-price"> $87.00 </Card.Text>
+                                    <Button className="card-shopbtn" variant="primary">ADD TO CART</Button>
+                                </Card.Body>
+                            </Card>
+
+                        </Col>
                     </Row>
+                    <Row className="shop-boxTwo mt-3 ">
+                        <Col className="d-flex justify-content-center" lg={4} md={4} sm={4} >
+                            <Card className="card-sWhite">
+                                <div className="card-sImg card-sImgFour">
+                                
+                                </div>
+                               
+                                <Card.Body>
+                                    <Card.Title className="card-head">Wedding Bouquet</Card.Title>
+                                    <Card.Text className="card-price"> $112.00 </Card.Text>
+                                    <Button className="card-shopbtn" variant="primary">ADD TO CART</Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="d-flex justify-content-center" lg={4} md={4} sm={4} >
+                            <Card className="card-sWhite">
+                                <div className="card-sImg card-sImgFive">
+                                
+                                </div>
+                               
+                                <Card.Body>
+                                    <Card.Title className="card-head">Wedding Candles</Card.Title>
+                                    <Card.Text className="card-price"> $45.00 </Card.Text>
+                                    <Button className="card-shopbtn"  variant="primary">ADD TO CART</Button>
+                                </Card.Body>
+                            </Card>
+
+                        </Col>
+                        <Col className="d-flex justify-content-center" lg={4} md={4} sm={4}>
+                            <Card className="card-sWhite" >
+                                <div className="card-sImg card-sImgSix">
+                               
+                                </div>
+                                
+                                <Card.Body>
+                                    <Card.Title className="card-head">Wedding Lighted Signs</Card.Title>
+                                    <Card.Text className="card-price"> $130.00 </Card.Text>
+                                    <Button className="card-shopbtn" variant="primary">ADD TO CART</Button>
+                                </Card.Body>
+                            </Card>
+
+                        </Col>
+                    </Row>
+
+
+
+
+
                 </Col>
             </Row>
         </Container>
